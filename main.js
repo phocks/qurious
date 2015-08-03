69342c8d6f885bfb57d6a0fb4019019b2dd1abb7
Joshua Byrd <phocks@gmail.com>
2015-08-03 17:04:49 +1000

    trying to get this working

--- main.js
+++ main.js
@@ -251,7 +251,7 @@ if (Meteor.isServer) {
       Counters.insert( { _id: "quote_id", seq: 0 } );
     }
 
-    process.env.HTTP_FORWARDED_COUNT = 1;
+    //process.env.HTTP_FORWARDED_COUNT = 1;
 
     
 
@@ -267,6 +267,8 @@ if (Meteor.isServer) {
     Meteor.onConnection(function(conn) {
       console.log(conn.clientAddress);
     });
+
+
 
 
   // Get the server to publish our collections

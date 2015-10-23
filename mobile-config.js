b05273dcc3c9e3a0be9bf8e7daaedab4a9af1932
Joshua Byrd <phocks@gmail.com>
2015-10-23 14:01:47 +1000

    ios things

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,10 @@
 // This section sets up some basic app metadata,
 // the entire section is optional.
 
+// App.accessRule('*'); allows access to other sites.
+// Consider changing this to only allow Google Fonts etc as needed
 App.accessRule('*');
 
+// Overlay the background color over the status bar
+// Watch out that the content doesn't bunch up.
 App.setPreference('StatusBarOverlaysWebView', 'true');

cb7ebd901c6d1f7670514ed4a4f533c68a52199c
Joshua Byrd <phocks@gmail.com>
2015-10-23 13:37:28 +1000

    commenting out icons until they are created

--- mobile-config.js
+++ mobile-config.js
@@ -11,14 +11,14 @@ App.info({
 
 // Set up resources such as icons and launch screens.
 App.icons({
-  'iphone': 'icons/icon-60.png',
-  'iphone_2x': 'icons/icon-60@2x.png',
+  // 'iphone': 'icons/icon-60.png',
+  // 'iphone_2x': 'icons/icon-60@2x.png',
   // ... more screen sizes and platforms ...
 });
 
 App.launchScreens({
-  'iphone': 'splash/Default~iphone.png',
-  'iphone_2x': 'splash/Default@2x~iphone.png',
+  // 'iphone': 'splash/Default~iphone.png',
+  // 'iphone_2x': 'splash/Default@2x~iphone.png',
   // ... more screen sizes and platforms ...
 });
 

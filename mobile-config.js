1ac1f0e2f62142cfd1d1db3bba486516f6ed73cc
Joshua Byrd <phocks@gmail.com>
2015-10-23 13:51:20 +1000

    background color

--- mobile-config.js
+++ mobile-config.js
@@ -2,3 +2,8 @@
 // the entire section is optional.
 
 App.accessRule('*');
+
+App.setPreference('BackgroundColor', '#fdfbf8');
+App.setPreference('HideKeyboardFormAccessoryBar', true);
+App.setPreference('Orientation', 'default');
+App.setPreference('Orientation', 'all', 'ios');

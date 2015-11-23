fc9d1004e37bc6139462dde46902658f421d7a04
Joshua Byrd <phocks@gmail.com>
2015-11-23 12:06:44 +1000

    Moving publications to own file
    Also, renaming serverMethods as server is implied and redundant by the
    folder. Also an update.

--- main.js
+++ main.js
@@ -437,80 +437,8 @@ if (Meteor.isServer) {
 
 
 
-
-  // Get the server to publish our collections
-  // Basically we only want to publish the quotes from our server which we are
-  // actually interested in using on the client side.
-  Meteor.publish("quotesAll", function () {
-    return Quotes.find({}, { sort: {createdAt: -1} });
-  });
-
-
-  Meteor.publish("quotesLatest", function (limit) {
-    if (limit > Quotes.find().count()) {
-      limit = 0;
-    }
-    return Quotes.find({}, { sort: {createdAt: -1}, limit: limit });
-  });
-
-
-  Meteor.publish("quotesPopular", function (limit) {
-    if (limit > Quotes.find().count()) {
-      limit = 0;
-    }
-
-    return Quotes.find({}, { sort: {views: -1, upcount: -1}, limit: limit });
-  });
-
-
-  Meteor.publish("quotesCurrentUser", function () {
-    return Quotes.find({ owner: this.userId });
-  });
-
-
-  Meteor.publish("quotesSlugUser", function (user_slug) {
-    check(user_slug, String);
-    return Quotes.find({ username: user_slug }, { sort: {createdAt: -1}});
-  });
-
-
-  Meteor.publish("quotesSlug", function (slug) {
-    check(slug, String);
-    var quote = Quotes.find({ _id: slug });
-    // console.log(quote); // trying to get counter quotes working too../....
-    // if (!quote) quote = Quotes.find({ quote_id: slug });
-    return quote;
-  });
-
-  // Pusblish quotes given IDs in an array as input
-  Meteor.publish("quotesInArray", function (array) {
-    return Quotes.find({ _id: { $in: array } }); // , {sort: {createdAt: -1}} taken out as test
-  });
-
-
-  Meteor.publish("counters", function () {
-    return Counters.find();
-  });
-
-  // We are going to publish some more userData
-  // in order to check if user is admin we need this
-  Meteor.publish("userData", function () {
-    return Meteor.users.find({},
-      { fields: {'admin':1, 'liked': 1, 'username': 1 }
-    });
-
-
-    /*if (this.userId) {
-      return Meteor.users.find({_id: this.userId},
-        { fields: {'admin': 1, 'liked': 1, 'username': 1 }
-      });
-    } else {
-      return Meteor.users.find({},
-        { fields: {'liked': 1, 'username': 1 }
-      });
-      this.ready();
-    }*/
-  });
+  // Meteor publish publication functions moved to /server/pulbications.js
+  
 
 
 } // end of the server only code

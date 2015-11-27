4a19758a5f62ac50100216c1aa3f3865e304666b
Joshua Byrd <phocks@gmail.com>
2015-11-27 23:17:37 +1000

    Finally almost flawlessly got dropcaps responsive in js
    This was tricky

--- lite/lite.js
+++ lite/lite.js
@@ -103,28 +103,60 @@ Router.route('/:_author_slug', {
 
 
 
+
+
+
 if (Meteor.isClient) {
   // Here we are experimenting with Dropcaps
   // This adds a span to the first letter so we can style it
 
-  Template.LiteQuote.onRendered(function () {
-      console.log('Inserting dropcaps span');
-      var node = $("p").contents().filter(function () { return this.nodeType == 3 }).first(),
-          text = node.text().trim(),
-          first = text.slice(0, 1);
-      
-      if (!node.length) {
-          console.log('not done');
-          return;
-        }
-      
-      node[0].nodeValue = text.slice(first.length);
-      node.before('<span id="dropcap">' + first + '</span>');
+  // A little trick to sense window resizing
+  // Added another little hack to wait for a bit
+  // to stop a million events triggering
+  /* We are doing this another way now using matchMedia
+  Meteor.startup(function() {
+    
 
-      var dropcap = document.getElementById("dropcap");
-      window.Dropcap.layout(dropcap, 2.4, 2);
+      function resizedw(){
+          // Haven't resized in 100ms!
+          Session.set("windowResizedAt", new Date());
+      }
+
+      var doit;
+      window.onresize = function(){
+        clearTimeout(doit);
+        doit = setTimeout(resizedw, 100);
+      };
+
+      
+    
   });
 
+*/
+  
+
+  // Dropcaps for Quotes
+  Template.LiteQuote.onRendered(function () {
+
+    console.log('Inserting dropcaps span');
+    var node = $("p").contents().filter(function () { return this.nodeType == 3 }).first(),
+        text = node.text().trim(),
+        first = text.slice(0, 1);
+    
+    if (!node.length) {
+        console.log('not done');
+        return;
+      }
+    
+    node[0].nodeValue = text.slice(first.length);
+    node.before('<span id="dropcap">' + first + '</span>');
+
+    var dropcap = document.getElementById("dropcap");
+    window.Dropcap.layout(dropcap, 2.2, 2);
+  
+  });
+
+  // Dropcaps for Homepage text
   Template.LiteHome.onRendered(function () {
       console.log('Inserting dropcaps span');
       var node = $("p").contents().filter(function () { return this.nodeType == 3 }).first(),
@@ -139,9 +171,98 @@ if (Meteor.isClient) {
       node[0].nodeValue = text.slice(first.length);
       node.before('<span id="dropcap">' + first + '</span>');
 
-      // $(window).resize(function(evt) {
-        var dropcap = document.getElementById("dropcap");
-        window.Dropcap.layout(dropcap, 2.4, 2);
-      // });
+      
+      var dropcap = document.getElementById("dropcap");
+      window.Dropcap.layout(dropcap, 2.2, 2);
+      
   });
-}
+
+
+  // Media queries for javascript pretty much
+  // screenSize = "mobile";
+
+
+/*
+  Tracker.autorun(function () {
+    var touched = Session.get('windowResizedAt');
+
+    // Session.set("screenSize", "mobile");
+
+    // if above mobile set to tablet
+    var mq = window.matchMedia( "(min-width: 768px)" );
+    if (mq.matches) {
+      // Session.set("screenSize", "tablet");
+      if (screenSize != "tablet") {
+        screenSize = "tablet";
+        console.log(screenSize);
+        return false;
+      }
+    }
+    
+
+    // if above tablet set to desktop
+    mq = window.matchMedia( "(min-width: 922px)" );
+    if (mq.matches) {
+      // Session.set("screenSize", "desktop");
+      if (screenSize != "desktop") {
+        screenSize = "desktop";
+        console.log(screenSize);
+        return false;
+      }
+    }
+    
+
+    // if above desktop set to large-desktop
+    mq = window.matchMedia( "(min-width: 1200px)" );
+    if (mq.matches) {
+      // Session.set("screenSize", "large-desktop");
+      if (screenSize != "large-desktop") {
+        screenSize = "large-desktop";
+        console.log(screenSize);
+        return false;
+      }
+    }
+    else {
+      screenSize = "mobile";
+      console.log(screenSize);
+    }
+
+    
+
+
+
+    
+  });*/
+
+
+
+  // Finally got it working
+  var tablet = window.matchMedia("(min-width: 768px)");
+  var desktop = window.matchMedia("(min-width: 992px)");
+  var largeDesktop = window.matchMedia("(min-width: 1200px)");  
+
+  var handleMediaChange = function (mediaQueryList) {
+      if (mediaQueryList.matches) {
+        console.log("Media query greater than triggered")
+        var dropcap = document.getElementById("dropcap");
+        window.Dropcap.layout(dropcap, 2.2, 2);
+      }
+      else {
+        // The browser window is less than 480px wide
+        console.log("Media query js smaller than triggered")
+        var dropcap = document.getElementById("dropcap");
+        window.Dropcap.layout(dropcap, 2.2, 2);
+      }
+  }
+
+
+  tablet.addListener(handleMediaChange);
+  desktop.addListener(handleMediaChange);
+  largeDesktop.addListener(handleMediaChange);
+  handleMediaChange(tablet);  
+  handleMediaChange(desktop);  
+  handleMediaChange(largeDesktop);
+
+
+
+} // end client only

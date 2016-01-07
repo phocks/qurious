4453a4a40819a2a51365f0775deaa028830dda0e
Joshua Byrd <phocks@gmail.com>
2016-01-07 19:21:46 +1000

    Search box on each quote evolution.

--- main.js
+++ main.js
@@ -41,6 +41,13 @@ Words = new Mongo.Collection('words'); // Words are the basis of ideas
 loadMoreLimit = 5;  // for infinite scrolling, how many per load
 maximumQuotationLength = 1000; // in characters
 
+// Deny public from editing profile. May prevent DoS attack
+Meteor.users.deny({
+  update: function() {
+    return true;
+  }
+});
+
 
 
 
@@ -194,24 +201,24 @@ if (Meteor.isClient) { // only runs on the client
   // This is not a very elegant way of doing it. Please change in future
 
 
-  incrementLimit = function(inc) { // this is defining a new global function
-    var inc = loadMoreLimit;
-    newLimit = Session.get('limit') + inc;
-    Session.set('limit', newLimit);
-  }
+  // incrementLimit = function(inc) { // this is defining a new global function
+  //   var inc = loadMoreLimit;
+  //   newLimit = Session.get('limit') + inc;
+  //   Session.set('limit', newLimit);
+  // }
 
-  Template.Quotes.created = function() {
-    Session.set('limit', loadMoreLimit);  // use Session.setDefault maybe
+  // Template.Quotes.created = function() {
+  //   Session.set('limit', loadMoreLimit);  // use Session.setDefault maybe
 
-    // Deps.autorun() automatically rerun the subscription whenever Session.get('limit') changes
-    // http://docs.meteor.com/#deps_autorun
-    // Changed to 'Tracker' in newer versions of Meteor
-    Tracker.autorun(function() {
-      Meteor.subscribe('quotesPopular', Session.get('limit'));
-      Meteor.subscribe('quotesLatest', Session.get('limit'));
-      Meteor.subscribe('quotesCurrentUser', Session.get('limit'));
-    });
-  }
+  //   // Deps.autorun() automatically rerun the subscription whenever Session.get('limit') changes
+  //   // http://docs.meteor.com/#deps_autorun
+  //   // Changed to 'Tracker' in newer versions of Meteor
+  //   Tracker.autorun(function() {
+  //     Meteor.subscribe('quotesPopular', Session.get('limit'));
+  //     Meteor.subscribe('quotesLatest', Session.get('limit'));
+  //     Meteor.subscribe('quotesCurrentUser', Session.get('limit'));
+  //   });
+  // }
 
   // This is an auto load feature when we have reached the bottom
   /*
@@ -238,34 +245,14 @@ if (Meteor.isClient) { // only runs on the client
 
   // Here are the helpers to put data into Templates etc
 
-  Template.SingleQuote.helpers({
-    // determines if user submitted quote
-    isOwner: function () {
-      var quoteId = Session.get("sessionQuoteId");
-      var currentQuote = Quotes.findOne({ _id: quoteId });
-
-      if (currentQuote.owner == Meteor.userId()) {
-        console.log("The current user has submitted this quote.")
-        return true;
-      }
-      else return false;
-    },
-    // works out if user has dogeared quote or not
-    dogeared: function () {
-      if (!Meteor.user()) return false; // if not logged in just undogear
-      var quoteId = Session.get("sessionQuoteId");
-      var user = Meteor.users.findOne({_id:Meteor.userId(), liked:{ $ne:quoteId }});
-      if (user) return false;
-      else return true;
-    }
-  });
+  
 
 
-  Template.ListAuthors.helpers({
-    authors: function () {
-      return Authors.find({}, {sort: {name: 1}});
-    }
-  });
+  // Template.ListAuthors.helpers({
+  //   authors: function () {
+  //     return Authors.find({}, {sort: {name: 1}});
+  //   }
+  // });
 
 
 
@@ -299,6 +286,29 @@ if (Meteor.isClient) { // only runs on the client
 
 // Events that drive things like clicks etc
 
+
+  Template.SingleQuote.helpers({
+    // determines if user submitted quote
+    isOwner: function () {
+      var quoteId = Session.get("sessionQuoteId");
+      var currentQuote = Quotes.findOne({ _id: quoteId });
+
+      if (currentQuote.owner == Meteor.userId()) {
+        console.log("The current user has submitted this quote.")
+        return true;
+      }
+      else return false;
+    },
+    // works out if user has dogeared quote or not
+    dogeared: function () {
+      if (!Meteor.user()) return false; // if not logged in just undogear
+      var quoteId = Session.get("sessionQuoteId");
+      var user = Meteor.users.findOne({_id:Meteor.userId(), liked:{ $ne:quoteId }});
+      if (user) return false;
+      else return true;
+    }
+  });
+
   // Let's finally set up a delete
   Template.SingleQuote.events({
     "click .delete-click": function () {
@@ -317,17 +327,7 @@ if (Meteor.isClient) { // only runs on the client
   });
 
 
-  // this isn't even used any more but yeah it's for a delete button on Explore
-  Template.Quotes.events({
-    "click .delete-click": function () {
-      Meteor.call('deleteQuote', this._id);
-    },
-
-
-    /*"click .list-quote": function () {
-      Router.go('/quotes/' + this._id);
-    } this was commented out in favour of a direct read more link */
-  });
+  
 
 
 
@@ -357,25 +357,7 @@ if (Meteor.isClient) { // only runs on the client
   });
 
 
-    Template.ListAuthors.events({
-      "submit .new-author": function (event) {      
-        var author = event.target.author.value;
-        if (author == "") return false; // prevent empty strings
-
-        Meteor.call('addAuthor', author);
-
-        // Clear form      
-        event.target.author.value = "";
-
-        // Prevent default action from form submit
-        return false;
-      },
-      "click .delete": function () {
-        if (confirm('Really delete ?')) {
-          Meteor.call('deleteAuthor', this._id);
-        }
-      }
-    });
+  
 
 
 
@@ -401,27 +383,48 @@ if (Meteor.isClient) { // only runs on the client
 
 
     Template.LiteQuote.events({
-      "submit .new-word": function (event) {      
-        var word = event.target.word.value;
-        var quote_id = Session.get('sessionQuoteId');
-        console.log(quote_id);
-        if (word == "") return false; // prevent empty strings
-
-        Meteor.call('addWordToQuote', word, quote_id);
-
-        // Clear form      
-        event.target.word.value = "";
-
-        // Prevent default action from form submit
+      "submit .quotePageAnother": function (event) {      
+      var word = event.target.word.value;
+      var quote_id = Session.get('sessionQuoteId');
+      console.log("Current quote ID: " + quote_id);
+      if (word == "") {
+        Router.go("/random");
         return false;
-      }, 
-      "click .delete": function () {
-        var word = this.toString();
-        var quote_id = Session.get('sessionQuoteId');
-        if (confirm('Really delete ?')) {
-          Meteor.call('deleteWordFromQuote', word, quote_id);
-        }
-      }     
+      }
+      else {
+        Session.set('currentWord', word);
+        Router.go("/random/" + word);
+      }
+
+           
+
+      // Prevent default action from form submit
+      return false;
+
+    }
+
+
+      // "submit .new-word": function (event) {      
+      //   var word = event.target.word.value;
+      //   var quote_id = Session.get('sessionQuoteId');
+      //   console.log(quote_id);
+      //   if (word == "") return false; // prevent empty strings
+
+      //   Meteor.call('addWordToQuote', word, quote_id);
+
+      //   // Clear form      
+      //   event.target.word.value = "";
+
+      //   // Prevent default action from form submit
+      //   return false;
+      // }, 
+      // "click .delete": function () {
+      //   var word = this.toString();
+      //   var quote_id = Session.get('sessionQuoteId');
+      //   if (confirm('Really delete ?')) {
+      //     Meteor.call('deleteWordFromQuote', word, quote_id);
+      //   }
+      // }
     });
 
 
@@ -455,26 +458,7 @@ if (Meteor.isClient) { // only runs on the client
 
 
 
-// Template.Header.events({
-    //   "submit .search-form": function (event) {
-    //     var q = event.target.q.value;
 
-    //     if (q == "") {
-    //       Router.go('/explore/latest');
-    //       return false;
-
-    //     }
-
-    //     //event.target.q.value = "";
-
-
-    //     Router.go('/search/' + q);
-
-
-    //     // Prevent default action from form submit
-    //     return false;
-    //   },
-    // });
 
 
 
@@ -539,6 +523,63 @@ if (Meteor.isClient) { // only runs on the client
     desktop.addListener(handleMediaChange);
     largeDesktop.addListener(handleMediaChange);
   });
+
+
+
+// Template.ListAuthors.events({
+//   "submit .new-author": function (event) {      
+//     var author = event.target.author.value;
+//     if (author == "") return false; // prevent empty strings
+
+//     Meteor.call('addAuthor', author);
+
+//     // Clear form      
+//     event.target.author.value = "";
+
+//     // Prevent default action from form submit
+//     return false;
+//   },
+//   "click .delete": function () {
+//     if (confirm('Really delete ?')) {
+//       Meteor.call('deleteAuthor', this._id);
+//     }
+//   }
+// });
+
+
+// Template.Header.events({
+//   "submit .search-form": function (event) {
+//     var q = event.target.q.value;
+
+//     if (q == "") {
+//       Router.go('/explore/latest');
+//       return false;
+
+//     }
+
+//     //event.target.q.value = "";
+
+
+//     Router.go('/search/' + q);
+
+
+//     // Prevent default action from form submit
+//     return false;
+//   },
+// });
+
+
+// this isn't even used any more but yeah it's for a delete button on Explore
+  // Template.Quotes.events({
+  //   "click .delete-click": function () {
+  //     Meteor.call('deleteQuote', this._id);
+  //   },
+
+
+  //   "click .list-quote": function () {
+  //     Router.go('/quotes/' + this._id);
+  //   } this was commented out in favour of a direct read more link 
+  // });
 
 
 
